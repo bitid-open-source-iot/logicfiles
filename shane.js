bcc5104a5b65bb2f60f61719e9ab6417e862a9b5
Shane Bowyer <sbdurbs@gmail.com>
2021-10-03 13:01:25 +0200

    Update shane.js

--- shane.js
+++ shane.js
@@ -43,7 +43,7 @@ function shane1(req) {
 function shane2(req) {
     var deferred = Q.defer()
     let payload = req.body.payload
-    payload.AI1 = parseInt(payload.AI1) * 2
+    payload.AI1 = parseInt(payload.AI1) * 3
     deferred.resolve({payload: payload})
     return deferred.promise
 }
