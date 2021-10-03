3bd8e5e8c03a54dbf2e4fe0d989d19b301588930
Shane Bowyer <sbdurbs@gmail.com>
2021-10-03 13:49:15 +0200

    divide 2

--- shane.js
+++ shane.js
@@ -43,7 +43,7 @@ function shane1(req) {
 function shane2(req) {
     var deferred = Q.defer()
     let payload = req.body.payload
-    payload.AI1 = parseInt(payload.AI1) * 1000000
+    payload.AI1 = parseInt(payload.AI1) / 2
     deferred.resolve({payload: payload})
     return deferred.promise
 }
